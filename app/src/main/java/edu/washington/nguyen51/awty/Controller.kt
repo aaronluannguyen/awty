011a2a6f40cf6007b49e39e4c98f70a639d08698
Student User <studentuser@Students-MacBook-Pro.local>
2018-05-07 02:51:31 -0700

    Finished starting functionality with checking of valid inputs

--- app/src/main/java/edu/washington/nguyen51/awty/Controller.kt
+++ app/src/main/java/edu/washington/nguyen51/awty/Controller.kt
@@ -6,3 +6,35 @@ fun setStartOrStopBtn(start: Boolean): String {
         false -> return "Stop"
     }
 }
+
+fun checkPhoneNumber(num: String): Boolean {
+    if (num.length == 10) {
+        return true
+    }
+    return false
+}
+
+fun checkMessage(message: String): Boolean {
+    if (message != "") {
+        return true
+    }
+    return false
+}
+
+fun checkMinuteInterval(time: String): Boolean {
+    if (time != "") {
+        return true
+    }
+    return false
+}
+
+fun checkInputFieldsAreValid(phone: String, message: String, time:String): Boolean {
+    var phoneInput = checkPhoneNumber(phone)
+    var messageInput = checkMessage(message)
+    var timeInput = checkMinuteInterval(time)
+
+    if (phoneInput == false || messageInput == false || timeInput == false) {
+        return false
+    }
+    return true
+}
--- app/src/main/java/edu/washington/nguyen51/awty/MainActivity.kt
+++ app/src/main/java/edu/washington/nguyen51/awty/MainActivity.kt
@@ -2,6 +2,9 @@ package edu.washington.nguyen51.awty
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +14,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var startBool = true
-        btnStartStop.isClickable = false
+        var ready = false
         btnStartStop.setText(setStartOrStopBtn(startBool))
 
+        btnStartStop.setOnClickListener {
+            if (startBool) {
+                // Check if clickable by making sure all fields are valid
+                // Once they are valid switch the startBool boolean to get start/stop swap on btn text
+                ready = checkInputFieldsAreValid(ToPhoneNumber.text.toString(), Message.text.toString(), MinuteInterval.text.toString())
+                Log.i("BTN STATUS", ready.toString())
+                Log.i("BTN STATUS", ToPhoneNumber.text.toString().length.toString())
+                if (ready) {
+                    startBool = !startBool
+                    var btnText = setStartOrStopBtn(startBool)
+                    btnStartStop.setText(btnText)
+                    // Run function that repeatedly sends toast messages for every x minute
+                }
+            } else {
 
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ToPhoneNumber.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        Message.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        MinuteInterval.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 }
 
